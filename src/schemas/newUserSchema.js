b9640ff466cd2534b81b3e2bd610ac4bf0d651e1
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-09 20:00:41 -0300

    ajusta newUserSchema para aceitar ultimo nome

--- src/schemas/newUserSchema.js
+++ src/schemas/newUserSchema.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 
 const newUserSchema = joi.object({
-    "name": joi.string().pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ]*$/).required(),
+    "name": joi.string().pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ]*[ ]{1}?[A-Za-zÀ-ÖØ-öø-ÿ]*?$/).required(),
     "email": joi.string().email().required(),
     "password": joi.string().required(),
     "image": joi.string().allow('').required()
