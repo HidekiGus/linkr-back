e1b74521e9d1568b35d0dd9a48b4ebb9cc34c7ac
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-09 19:55:16 -0300

    implementa validateSchema middleware e ajusta newUserSchema

--- src/schemas/newUserSchema.js
+++ src/schemas/newUserSchema.js
@@ -1,10 +1,10 @@
 import joi from 'joi';
 
 const newUserSchema = joi.object({
-    "name": "fulano",
+    "name": joi.string().pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ]*$/).required(),
     "email": joi.string().email().required(),
     "password": joi.string().required(),
-    "image": joi.string()
+    "image": joi.string().allow('').required()
 });
 
 export default newUserSchema;
