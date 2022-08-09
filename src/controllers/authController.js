968c109989a53c6f1e04109d3b93fbda511b8c34
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-09 19:33:29 -0300

    implementa verificação de email unico e encryptedPassword

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,17 @@
-import { insertNewUser } from "../repositories/authRepository.js";
+import bcrypt from 'bcrypt';
+import { findEmail, insertNewUser } from "../repositories/authRepository.js";
 
 export async function signUp(req, res) {
     try {
         const user = req.body;
+        const unique = await findEmail(user.email);
+        const encryptedPassword = bcrypt.hashSync(user.password, 5);
+
+        if(unique.rowCount !== 0) {
+            return res.status(409).send('Email já cadastrado')
+        }
         
-        await insertNewUser(user);
+        await insertNewUser(user, encryptedPassword);
         res.status(201).send('Usuário cadastrado com sucesso!');
     } catch (error) {
         res.status(500).send(error)
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -1,8 +1,12 @@
 import connection from '../dbStrategy/pg.js';
 
-export async function insertNewUser(user) {
+export async function insertNewUser(user, encryptedPassword) {
     return await connection.query(
         'INSERT INTO users (name, email, password, image) VALUES ($1, $2, $3, $4)',
-        [user.name, user.email, user.password, user.image]
+        [user.name, user.email, encryptedPassword, user.image]
     )
+}
+
+export async function findEmail(email) {
+    return await connection.query('SELECT * FROM users WHERE email = $1', [email])
 }
