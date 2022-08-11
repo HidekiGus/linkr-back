862df2db9d7393d449718d3c9f5a803d09c64231
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-10 21:35:25 -0300

    implementa postController

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { postPost } from "../controllers/postController.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import postSchema from "../schemas/postSchema.js";
 
 const postRouter = Router();
 
-postRouter.post('/post', validateSchema(postSchema));
+postRouter.post('/post', validateSchema(postSchema), postPost);
 
 export default postRouter;
