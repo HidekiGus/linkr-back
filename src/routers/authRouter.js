ac4376d0c944e014f4fc2898a2437bcb7ab49c37
daniel <daniel.bnh13@gmail.com>
2022-08-12 14:47:54 -0300

    adicionei sistemas de pesquisa de usuarios,curtidas,hashtags e hashtags trending

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { signUp } from "../controllers/authController.js";
 import { login } from "../controllers/Login/loginController.js";
-import { pesquisa,hashtag } from "../controllers/Timeline/ search.js";
+import { pesquisa,hashtag,hashtagsTrending,buscarUsuario} from "../controllers/Timeline/search.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import newUserSchema from "../schemas/newUserSchema.js";
 
@@ -11,6 +11,7 @@ authRouter.post('/signup', validateSchema(newUserSchema), signUp);
 authRouter.post('/signin',login)
 authRouter.get('/timeline', pesquisa)
 authRouter.get('/hashtag', hashtag)
-
+authRouter.get('/hashtagsTrending',hashtagsTrending)
+authRouter.get('/user/:id', buscarUsuario)
 
 export default authRouter;
