7ce5b927e9f16b7eea28a9c0236b4e243a5508d9
dr1co <dricomatoba@hotmail.com>
2022-08-11 22:10:38 -0300

    optimized queries

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -2,21 +2,18 @@ import { v4 as uuid } from 'uuid';
 import { getUserById, deletePreviousSession, generateNewSession } from '../repositories/authRepository.js';
 
 export async function createNewSession(req, res) {
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer', '');
+    const user = res.locals.user;
     const newToken = uuid();
 
     try {
-        const result = await getUserByToken(token);
-        await deletePreviousSession(token); 
-        await generateNewSession(result.rows[0].id, newToken);
+        await generateNewSession(user.id, newToken);
 
-        const user = {
-            ...result.rows[0],
+        const newUser = {
+            ...user,
             token: newToken
         };
 
-        res.status(200).send(user);
+        res.status(200).send(newUser);
     } catch (err) {
         res.status(500).send("createNewSession: " + err);
     }
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -15,20 +15,15 @@ export async function findUserByToken(token) {
     return await connection.query(`SELECT "userId" FROM sessions WHERE token = $1`, [token]);
 }
 
-export async function getUserByToken(token) {
-    return await connection.query(`
-        SELECT users.id, users.name, users.email, users.image FROM users
-        JOIN sessions ON sessions."userId" = users.id
-        WHERE sessions.token = $1
-    `,
-        [token]
-    );
-}
-
-export async function deletePreviousSession(id) {
-    return await connection.query(`DELETE * FROM sessions WHERE "userId = $1`, [id]);
-}
-
 export async function generateNewSession(id, token) {
-    return await connection.query(`INSERT INTO sessions ("userId", "token") VALUES ($1, $2)`, [id, token]);
+    return await connection.query(`
+        UPDATE sessions
+        SET token = $2
+        WHERE user_id = $1
+    `,
+        [
+            id,
+            token
+        ]
+    );
 }
