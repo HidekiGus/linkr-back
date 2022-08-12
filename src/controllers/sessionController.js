45fff81d56daa180b710c12942e1af1e9b15011b
dr1co <dricomatoba@hotmail.com>
2022-08-11 21:30:50 -0300

    added changes to sessionController

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -2,13 +2,14 @@ import { v4 as uuid } from 'uuid';
 import { getUserById, deletePreviousSession, generateNewSession } from '../repositories/authRepository.js';
 
 export async function createNewSession(req, res) {
-    const userId = res.locals.userId;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer', '');
     const newToken = uuid();
 
     try {
-        const result = await getUserById(userId);
-        await deletePreviousSession(userId); 
-        await generateNewSession(userId, newToken);
+        const result = await getUserByToken(token);
+        await deletePreviousSession(token); 
+        await generateNewSession(result.rows[0].id, newToken);
 
         const user = {
             ...result.rows[0],
