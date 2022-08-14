646a7fdd40a815db4e6b0bc3800d459e4304601b
HidekiGus <40206355+HidekiGus@users.noreply.github.com>
2022-08-14 07:35:28 -0300

    fix: arruma conflito

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import authRouter from "./authRouter.js";
-import postsRouter from "./postsRouter.js";
+import hashtagRouter from "./hashtagRouter.js";
+import postRouter from "./postRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
-router.use(postsRouter);
+router.use(postRouter);
+router.use(hashtagRouter);
 
 export default router;
