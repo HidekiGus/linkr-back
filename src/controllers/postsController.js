791b9a3ea967314fca83bb2f4501acf7727082f6
HidekiGus <40206355+HidekiGus@users.noreply.github.com>
2022-08-14 07:30:14 -0300

    feat: altera rota GET pra enviar os metadados relacionados ao link do post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,9 +1,29 @@
+import urlMetadata from "url-metadata";
 import { getAllPosts } from "../repositories/postsRepository.js";
 
 export async function getPosts(req, res) {
     try {
         const posts = await getAllPosts();
-        return res.status(200).send(posts.rows);
+        const postsWithMetadata = await Promise.all(posts.rows.map(async(row) => {
+            return await urlMetadata(`${row.link}`).then(
+                function (metadata) {
+                    const obj = {
+                        userImage: row.image,
+                        userName: row.name,
+                        userPostDescription: row.description,
+                        userPostLink: row.link,
+                        metadataTitle: metadata.title,
+                        metadataDescription: metadata.description,
+                        metadataImage: metadata.image
+                    }
+                    return obj;
+                }, 
+                function (error) {
+                    return error;
+                }
+            )
+        }));
+        return res.status(200).send(postsWithMetadata);
     } catch(error) {
         return res.status(500).send(error);
     }
--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,5 +1,12 @@
 import connection from "../dbStrategy/pg.js";
+import urlMetadata from "url-metadata";
 
 export async function getAllPosts() {
-    return await connection.query(`SELECT * FROM posts ORDER BY "createdAt" DESC LIMIT 20;`);
+    return await connection.query(`
+        SELECT users.name, users.image, posts.description, posts.link FROM users
+        JOIN posts
+        ON posts."user_id"=users.id
+        ORDER BY posts."createdAt" DESC
+        LIMIT 20;`
+    );
 }
