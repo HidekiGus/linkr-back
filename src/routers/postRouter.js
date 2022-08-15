1ed6f90bc73fee203919da8eb9a6545cce02884c
HidekiGus <40206355+HidekiGus@users.noreply.github.com>
2022-08-15 16:56:09 -0300

    Minor fixes and starts likes section

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { postPost } from "../controllers/postController.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import postSchema from "../schemas/postSchema.js";
-import {validateToken} from "../middlewares/validateToken.js"
+import {validateToken} from "../middlewares/validateToken.js";
 
 const postRouter = Router();
 
--- src/routers/postsRouter.js
+++ src/routers/postsRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getPosts } from "../controllers/postsController.js";
+import {validateToken} from "../middlewares/validateToken.js"
 
 const postsRouter = Router();
 
-postsRouter.get("/posts", getPosts);
+postsRouter.get("/posts", validateToken, getPosts);
 
 export default postsRouter;
