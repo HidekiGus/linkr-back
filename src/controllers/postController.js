a37f9a95c0e6c4f5ddafba7d6c7836bb56f2a0d2
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-16 16:44:22 -0300

    unifica postRouter e postController

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,3 +1,5 @@
+import urlMetadata from "url-metadata";
+import { getAllPosts } from "../repositories/postsRepository.js";
 import { insertPost } from '../repositories/postRepository.js'
 
 export async function postPost(req, res) {
@@ -8,5 +10,34 @@ export async function postPost(req, res) {
         res.status(201).send('Post realizado.');
     } catch (error) {
         res.status(500).send(error)
+    }
+}
+
+export async function getPosts(req, res) {
+    try {
+        const posts = await getAllPosts();
+        const postsWithMetadata = await Promise.all(posts.rows.map(async(row) => {
+            return await urlMetadata(`${row.link}`).then(
+                function (metadata) {
+                    const obj = {
+                        userImage: row.image,
+                        userName: row.name,
+                        userPostDescription: row.description,
+                        userPostLink: row.link,
+                        metadataTitle: metadata.title,
+                        metadataDescription: metadata.description,
+                        metadataImage: metadata.image
+                    }
+                    return obj;
+                }, 
+                function (error) {
+                    return error;
+                }
+            )
+        }));
+        return res.status(200).send(postsWithMetadata);
+    } catch(error) {
+        console.log(error)
+        return res.status(500).send(error);
     }
 }
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,11 @@ import {Router} from "express";
 import authRouter from "./authRouter.js";
 import hashtagRouter from "./hashtagRouter.js";
 import postRouter from "./postRouter.js";
-import postsRouter from "./postsRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(postRouter);
-router.use(postsRouter);
 router.use(hashtagRouter);
 
 export default router;
